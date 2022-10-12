3bfa791b608c0df0f74fa60c7d970ca45d1285a0
alexandrabaturina <alexandra.v.baturina@gmail.com>
2022-10-12 16:34:59 -0700

    chore: add mock database

---
+++ src/Components/DBMenu.js
@@ -0,0 +1,96 @@
+const dbMenu = {
+	burger: [
+		{
+			name: "JS Burger",
+			img: "/menu/js-burger.png",
+			id: 1,
+			price: 6.0,
+			toppings: [
+				"Bacon",
+				"Ketchup",
+				"Cheddar Cheese",
+				"Pickles",
+				"Tomatoes",
+				"Jalapeño",
+			],
+		},
+		{
+			name: "Borsh Burger",
+			img: "/menu/borsh-burger.png",
+			id: 2,
+			price: 6.0,
+			toppings: ["Bacon", "Red Onions"],
+		},
+		{
+			name: "Freckles Burger",
+			img: "/menu/freckles-burger.png",
+			id: 3,
+			price: 5.5,
+			toppings: ["Pinapple Slices", "Mayo"],
+		},
+		{
+			name: "Timon Burger",
+			img: "/menu/black-burger.png",
+			id: 4,
+			price: 4.75,
+		},
+		{
+			name: "React Burger",
+			img: "/menu/react-burger.png",
+			id: 5,
+			price: 5.5,
+			toppings: ["Ketchup", "Cheddar Cheese"],
+		},
+		{
+			name: "Sun Burger",
+			img: "/menu/sun-burger.png",
+			id: 6,
+			price: 4.5,
+			toppings: ["Cheddar Cheese", "Red Onions"],
+		},
+	],
+	other: [
+		{
+			name: "Soft Drinks",
+			img: "/menu/soda.png",
+			id: 1,
+			price: 2.0,
+			choices: ["Coca-Cola", "Fanta", "Mirinda", "Sprite"],
+		},
+		{
+			name: "Tea",
+			img: "/menu/tea.png",
+			id: 2,
+			price: 1.0,
+			choices: ["Black", "Herbal"],
+		},
+		{
+			name: "French Fries",
+			img: "/menu/french-fries.png",
+			id: 3,
+			price: 2.5,
+		},
+		{
+			name: "Chicken Nuggets",
+			img: "/menu/nuggets.png",
+			id: 4,
+			price: 4.0,
+			choices: ["Fillet", "Wings"],
+		},
+		{
+			name: "Coffee",
+			img: "/menu/coffee.png",
+			id: 5,
+			price: 4.5,
+			choices: ["Espresso", "Cappuccino"],
+		},
+		{
+			name: "Grinch",
+			img: "/menu/green-coffee.png",
+			id: 6,
+			price: 4.0,
+		},
+	],
+}
+
+export default dbMenu
