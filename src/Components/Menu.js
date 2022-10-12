a1f6123f300c99dba16ad8429c9210205d46891a
alexandrabaturina <alexandra.v.baturina@gmail.com>
2022-10-12 16:54:10 -0700

    feat: display menu banner

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -2,6 +2,13 @@ import styled from "styled-components"
 import dbMenu from "./DBMenu"
 import { ListItem } from "./ListItem"
 
+const MenuBanner = styled.div`
+	background-image: url("/menu/banner.png");
+	background-size: cover;
+	width: 100%;
+	height: 250px;
+`
+
 const MenuStyled = styled.main`
 	background-color: #ccc;
 	margin-top: 80px;
@@ -12,15 +19,18 @@ const SectionMenu = styled.section`
 `
 
 export const Menu = () => (
-	<MenuStyled>
-		<SectionMenu>
-			<h2>Burgers</h2>
-			<ListItem itemList={dbMenu.burger} />
-		</SectionMenu>
+	<>
+		<MenuBanner />
+		<MenuStyled>
+			<SectionMenu>
+				<h2>Burgers</h2>
+				<ListItem itemList={dbMenu.burger} />
+			</SectionMenu>
 
-		<SectionMenu>
-			<h2>Appetizers / Drinks</h2>
-			<ListItem itemList={dbMenu.other} />
-		</SectionMenu>
-	</MenuStyled>
+			<SectionMenu>
+				<h2>Appetizers / Drinks</h2>
+				<ListItem itemList={dbMenu.other} />
+			</SectionMenu>
+		</MenuStyled>
+	</>
 )
