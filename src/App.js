27605a08b852b864a3893ec7c1da8ba661bbba66
alexandrabaturina <alexandra.v.baturina@gmail.com>
2022-10-12 14:51:32 -0700

    feat: display navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,58 @@
-import './App.css'
+import { createGlobalStyle } from "styled-components"
+import { NavBar } from "./Components/NavBar"
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: inherit;
+  }
+
+  body {
+      margin: 0;
+      background-golor: #fcfbfc;
+      font-family: Roboto;
+      font-size: 20px;
+      color: black
+    }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  h1, h2, h3 {
+    font-family: Pacifico;
+    padding: 0;
+    margin: 0;
+  }
+
+  p {
+    padding: 0;
+    margin: 0;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+`
 
 const App = () => (
-  <div>
-    Burger Store
-  </div>
+  <>
+    <GlobalStyle />
+    <NavBar />
+  </>
 )
 
 export default App
---
+++ src/Components/NavBar.js
@@ -0,0 +1,55 @@
+import styled from "styled-components"
+import logoImg from "../img/logo.svg"
+
+const NavBarStyled = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 999;
+  height: 80px;
+  width: 100vw;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  background-color: #998f07;
+  color: white;
+`
+
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const H1 = styled.h1`
+  font-size: 30px;
+  margin-left: 15px;
+`
+
+const ImgLogo = styled.img`
+  width: 50px;
+`
+
+const LogInBtn = styled.button`
+  width: 75px;
+  height: 30px;
+  color: #282a31;
+  font-size: 15px;
+  font-weight: 600;
+  border-radius: 5px;
+  border: 1px solid;
+  &:hover {
+    border: 2px solid;
+    background-color: #e7e9eb;
+  }
+`
+
+export const NavBar = () => (
+  <NavBarStyled>
+    <Logo>
+      <ImgLogo src={logoImg} alt="logo"></ImgLogo>
+      <H1>Mr. Donald's</H1>
+    </Logo>
+    <LogInBtn>Log In</LogInBtn>
+  </NavBarStyled>
+)
